dda537a7de4d7c32d396355523e1e2bdfd55b8b3
design4uMTDEV <design4u.dev@gmail.com>
2021-04-22 21:15:10 +0300

    Initialized controllers, models, and created migrations.

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function index() {
+        
+    }
+    public function show(Article $article) {
+        
+    }
+    public function edit(Article $article) {
+        
+    }
+    public function update(Article $article) {
+        
+    }
+    public function destroy(Article $article) {
+        
+    }
+}
